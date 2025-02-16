81757c1cd7642204adb8c847639e6ea074f871a7
kunal89204 <kunalkhandelwal108@gmail.com>
2025-02-16 15:44:57 +0530

    base url updated

--- frontend/src/api/query.ts
+++ frontend/src/api/query.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+    baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/v1`,
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',

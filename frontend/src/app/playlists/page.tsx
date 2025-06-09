857d1afd28755b1ee9cf4a5bec32457b789a50d1
kunal89204 <kunalkhandelwal108@gmail.com>
2025-06-09 15:33:12 +0530

    playist pre loader added

--- frontend/src/app/playlists/page.tsx
+++ frontend/src/app/playlists/page.tsx
@@ -1,17 +1,55 @@
 "use client";
 import Hero from "@/components/playlists/Hero";
 import Playlist from "@/components/playlists/Playlist";
-import { Box } from "@chakra-ui/react";
-import React from "react";
+import { Box, Flex, Spinner, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
 import usePlaylists from "@/hooks/usePlaylists";
+
 
 const Playlists: React.FC = () => {
   // Color schemes
 
   const { data, isLoading, error, isError } = usePlaylists();
 
+
+
   if (isLoading) {
-    return <div>Loading...</div>;
+    return  <Flex
+      className="w-full h-screen bg-gradient-to-br from-gray-900 via-black to-gray-900"
+      alignItems="center"
+      justifyContent="center"
+      position="relative"
+      overflow="hidden"
+    >
+      {/* Glowing Blurred Circle Background */}
+      <Box
+        className="absolute w-[300px] h-[300px] bg-purple-500/20 rounded-full blur-3xl animate-pulse"
+        style={{ top: "20%", left: "40%" }}
+      />
+      <Flex
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        zIndex="10"
+      >
+        <Spinner
+          thickness="3px"
+          speed="0.7s"
+          emptyColor="gray.700"
+          color="gray.400"
+          size="xl"
+        />
+        <Text
+          mt={4}
+          fontSize="lg"
+          color="gray.200"
+          letterSpacing="wide"
+          fontWeight="semibold"
+        >
+          Loading your playlists
+        </Text>
+      </Flex>
+    </Flex>
   }
 
   if (isError) {

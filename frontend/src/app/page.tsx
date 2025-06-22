9f90995b77252674ead506cb525f7ae321582f60
kunal89204 <kunalkhandelwal108@gmail.com>
2025-06-22 23:01:38 +0530

    skeleton added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import { myQuery } from "@/api/query";
 import React from "react";
@@ -198,7 +198,27 @@ const Home: React.FC = () => {
     return <div>Error</div>
   }
   if(isLoading){
-    return <div>Loading...</div>
+    return <Box className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 3xl:grid-cols-5">
+    {Array.from({ length: 8 }).map((_, i) => (
+      <Box key={i}>
+        <Skeleton
+          height="200px"
+          width="100%"
+          borderRadius={"14px"}
+          isLoaded={!isLoading}
+        />
+        <Flex py={2} gap={4}>
+          <SkeletonCircle size="10" isLoaded={!isLoading} />
+          <SkeletonText
+            height="20px"
+            width="250px"
+            noOfLines={3}
+            isLoaded={!isLoading}
+          />
+        </Flex>
+      </Box>
+    ))}
+  </Box>
   }
   return (
     <div>

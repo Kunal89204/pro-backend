a7b6c6c5c9c73cd9b151156243a8688e96c398f5
kunal89204 <kunalkhandelwal108@gmail.com>
2025-07-19 17:25:50 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ export const redis = new Redis({
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8000,'0.0.0.0', () => {
         console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
     })
 })
